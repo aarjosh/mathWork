0849338ddb48f283f0608f74f413ba62b01c8ded
aarjosh <aarjoshi27@gmail.com>
2023-04-09 13:32:40 -0700

    Added comment

--- triangles.py
+++ triangles.py
@@ -18,5 +18,7 @@ elif (b == 0) and (a != 0) and (c != 0):
 elif (c == 0) and (a != 0) and (b != 0): 
     c = math.sqrt(a**2 + b**2)
 
+# this program will give you all the sides of the triangle as
+# a=leg b=leg and c=hypotenuse
 
-print(f"all sides are: {a, b, c}")
+print(f"all sides are a, b, c : {a, b, c}")
