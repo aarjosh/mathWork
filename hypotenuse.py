dff16e3ad5ab0be485954f5bdb1317cbb88daf17
milapjoshi <milap.joshi@gmail.com>
2023-04-15 21:30:09 -0700

    Changes to add two ways (#1)

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,14 +1,15 @@
 import math
+
+print("We are here to help find side of triangles A and B are shorter sides of triangle and C is hypotenuse.")
 res = input("Are you trying to find the hypotenuse of the triangle.(Y/N)")
 if (res == "Y"):
-    else:
+    a = float(input("a: "))
+    b = float(input("b: "))
+    c = math.sqrt(a**2 + b**2)
+else:
     print("Input the lengths of the shorter triangle sides:")
-a = float(input("a: "))
-b = float(input("b: "))
-c = math.sqrt(a**2 + b**2)
-print("The length of the hypotenuse is:", c )
-print("Input the lengths of the shorter triangle sides:")
-a = float(input("a: "))
-b = float(input("b: "))
-c = math.sqrt(a**2 + b**2)
-print("The length of the hypotenuse is:", c )
+    a = float(input("shorter leg of triangle a: "))
+    c = float(input("Enter hypotenuse of triangle c: "))
+    b = math.sqrt(c**2 - a**2)
+
+print(f"The three sides of the right angle triangle are \n A: {a}\n B: {b}\n C: {c}")
